b4581ee52a100a1907dae1bbf48cda05445b2dfb
Xavi3R90 <javisanchezv90@gmail.com>
2020-11-18 18:27:53 -0300

    Estructura Creada - Models

---
+++ src/startup/container.js
@@ -0,0 +1,38 @@
+const { createContainer, asClass, asValue, asFunction } = require('awilix');
+
+// config
+const config = require('../config');
+const app = require('.');
+
+// services
+const { HomeService } = require('../services');
+
+// controllers
+const { HomeController } = require('../controllers');
+
+// routes
+const { HomeRoutes } = require('../routes/index.routes');
+const Routes = require('../routes');
+
+// models
+const { User, Comment, Idea } = require('../models');
+
+const container = createContainer();
+
+container.register({
+    app: asClass( app ).singleton(),
+    router: asFunction(Routes).singleton(),
+    config: asValue( config )
+}).register({
+    HomeService: asClass(HomeService).singleton()
+}).register({
+    HomeController: asClass(HomeController.bind(HomeController))
+}).register({
+    HomeRoutes: asFunction(HomeRoutes).singleton()
+}).register({
+    User: asValue(User),
+    Idea: asValue(Idea),
+    Comment: asValue(Comment)
+});
+
+module.exports = container;
